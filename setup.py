bc58fb0500fa1fd61b4b041e048862e05ed75dfd
basti <sebastian.gumprich@38.de>
2014-07-13 18:55:48 +0200

    added setup.py and pypi-upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py_time_widget',
+    version='1.0',
+    packages=['py_time_widget'],
+    url='https://github.com/rndmh3ro/py_time_widget',
+    license='GPL',
+    author='Sebastian Gumprich',
+    author_email='sebastian.gumprich@38.de',
+    description='Sends the current time to libnotify. Desktop widget',
+)
